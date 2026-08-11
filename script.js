431204854b27eea5ad3ee78f4a81a7c8e4525c3e
Jacob Maynard <jacob.d.maynard98@gmail.com>
2026-08-10 21:46:52 -0500

    Store weight entries in array

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+let weightEntries = [];
+
 const input = document.getElementById("weightInput");
 const button = document.getElementById("saveButton");
 const weightHistory = document.getElementById("weightHistory");
 
-// Take user input and add to unordered list in html
+// User Input added to html list, and weightEntries array.
 button.addEventListener("click", function() {
     const weight = input.value;
 
     const date = new Date();
     const formattedDate = date.toLocaleDateString();
+
+    const entry = {
+        date: formattedDate,
+        weight: Number(weight)
+    };
+    weightEntries.push(entry);
 
     const listItem = document.createElement("li");
 
